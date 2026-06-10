047060d228644e3449d9090847069dfd2aa18846
abhigna <bollam@gmail.com>
2026-06-10 13:31:10 +0530

    debug railway variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,11 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+console.log('DB_HOST=', process.env.DB_HOST);
+console.log('DB_PORT=', process.env.DB_PORT);
+console.log('DB_USER=', process.env.DB_USER);
+console.log('DB_NAME=', process.env.DB_NAME);
+
 const dbHost = process.env.DB_HOST || '127.0.0.1';
 const dbPort = process.env.DB_PORT || '3306';
 const dbUser = process.env.DB_USER || 'root';
